3e9f5cd803c762f33bb9f347850232295f4c7060
Ryan Flitcroft <ryanflitcroft@ymail.com>
2022-04-26 19:57:09 -0700

    init useEffect in app. Functions getCollection, handleData written in fetch-utils.

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,20 @@ import { useState, useEffect } from 'react';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
+import { getCollection } from './services/fetch-utils';
 
 export default function App() {
   const [collection, setCollection] = useState([]);
   const [collectionDisplay, setCollectionDisplay] = useState([]);
   const [search, setSearch] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function getData() {
+      await getCollection();
+    }
+    getData();
+  }, []);
 
   return (
     <>
---
+++ src/services/fetch-utils.js
@@ -0,0 +1,23 @@
+export async function getCollection() {
+  const res = await fetch(
+    'https://api.artic.edu/api/v1/artworks?fields=id,title,date_start,date_end,artist_title,artist_id,artwork_type_title&limit=100'
+  );
+
+  const { data } = await res.json();
+  const response = handleData(data);
+
+  return response;
+}
+
+function handleData(data) {
+  return data.map((item) => ({
+    id: item.id,
+    title: item.title,
+    artist: item.artist_title,
+    artistId: item.artist_id,
+    dateStart: item.date_start,
+    dateEnd: item.date_end,
+    artworkType: item.artwork_type_title,
+    imageId: item.image_id,
+  }));
+}
